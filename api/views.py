aca49ba3dedb8c3835d13d4c3d4deda9fe93f11b
xudoyberdi123 <71259765+xudoyberdi123@users.noreply.github.com>
2023-04-27 13:21:44 +0500

    methodism package updated

--- api/views.py
+++ api/views.py
@@ -7,9 +7,5 @@ from api.models.tokens import Token
 
 class PMView(METHODIZM):
     file = methods
-    token_key = "Bearer"
-    auth_headers = 'Authorization'
     token_class = Token
     not_auth_methods = settings.METHODS
-
-
